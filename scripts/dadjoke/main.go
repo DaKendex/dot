a7e21fc44fac2edcb19484159231dbd0378625de
Kenji Freitas <kfreitas@league.com>
2023-04-24 09:03:17 -0400

    updated dadjoke to go

---
+++ scripts/dadjoke/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Joke struct {
+	ID     string `json:"id"`
+	JOKE   string `json:"joke"`
+	STATUS int    `json:"status"`
+}
+
+func main() {
+	req, err := http.NewRequest("GET", "https://icanhazdadjoke.com", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	joke1 := Joke{}
+	jsonErr := json.Unmarshal(bytes, &joke1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	// dialog.Alert(joke1.JOKE)
+	fmt.Println(joke1.JOKE)
+}
