af27dae64c35a6ce6687963289baab1009de5270
Kenji Freitas <kfreitas@league.com>
2023-05-05 16:01:08 -0400

    updated

--- scripts/motd/motd.sh
+++ scripts/motd/motd.sh
@@ -2,17 +2,23 @@
 # ln -sf ${dotfiledir}/scripts/motd/motd.sh ${homedir}/.config/motd.sh
 
 # * ASCII head
-echo '          _              _            _                   _       _     '
-echo '         /\_\           /\ \         /\ \     _          /\ \    /\ \   '
-echo '        / / /  _       /  \ \       /  \ \   /\_\        \ \ \   \ \ \  '
-echo '       / / /  /\_\    / /\ \ \     / /\ \ \_/ / /        /\ \_\  /\ \_\ '
-echo '      / / /__/ / /   / / /\ \_\   / / /\ \___/ /        / /\/_/ / /\/_/ '
-echo '     / /\_____/ /   / /_/_ \/_/  / / /  \/____/_       / / /   / / /    '
-echo '    / /\_______/   / /____/\    / / /    / / //\ \    / / /   / / /     '
-echo '   / / /\ \ \     / /\____\/   / / /    / / / \ \_\  / / /   / / /      '
-echo '  / / /  \ \ \   / / /______  / / /    / / /  / / /_/ / /___/ / /__     '
-echo ' / / /    \ \ \ / / /_______\/ / /    / / /  / / /__\/ //\__\/_/___\    '
-echo ' \/_/      \_\_\\/__________/\/_/     \/_/   \/_______/ \/_________/    '
+# https://patorjk.com/software/taag/#p=display&f=Doh&t=KENDEX                                                                                                                   
+echo 'KKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNDDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEEXXXXXXX       XXXXXXX'
+echo 'K:::::::K    K:::::KE::::::::::::::::::::EN:::::::N       N::::::ND::::::::::::DDD   E::::::::::::::::::::EX:::::X       X:::::X'
+echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::::N      N::::::ND:::::::::::::::DD E::::::::::::::::::::EX:::::X       X:::::X'
+echo 'K:::::::K   K::::::KEE::::::EEEEEEEEE::::EN:::::::::N     N::::::NDDD:::::DDDDD:::::DEE::::::EEEEEEEEE::::EX::::::X     X::::::X'
+echo 'KK::::::K  K:::::KKK  E:::::E       EEEEEEN::::::::::N    N::::::N  D:::::D    D:::::D E:::::E       EEEEEEXXX:::::X   X:::::XXX'
+echo '  K:::::K K:::::K     E:::::E             N:::::::::::N   N::::::N  D:::::D     D:::::DE:::::E                X:::::X X:::::X   '
+echo '  K::::::K:::::K      E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N  D:::::D     D:::::DE::::::EEEEEEEEEE       X:::::X:::::X    '
+echo '  K:::::::::::K       E:::::::::::::::E   N::::::N N::::N N::::::N  D:::::D     D:::::DE:::::::::::::::E        X:::::::::X     '
+echo '  K:::::::::::K       E:::::::::::::::E   N::::::N  N::::N:::::::N  D:::::D     D:::::DE:::::::::::::::E        X:::::::::X     '
+echo '  K::::::K:::::K      E::::::EEEEEEEEEE   N::::::N   N:::::::::::N  D:::::D     D:::::DE::::::EEEEEEEEEE       X:::::X:::::X    '
+echo '  K:::::K K:::::K     E:::::E             N::::::N    N::::::::::N  D:::::D     D:::::DE:::::E                X:::::X X:::::X   '
+echo 'KK::::::K  K:::::KKK  E:::::E       EEEEEEN::::::N     N:::::::::N  D:::::D    D:::::D E:::::E       EEEEEEXXX:::::X   X:::::XXX'
+echo 'K:::::::K   K::::::KEE::::::EEEEEEEE:::::EN::::::N      N::::::::NDDD:::::DDDDD:::::DEE::::::EEEEEEEE:::::EX::::::X     X::::::X'
+echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::N       N:::::::ND:::::::::::::::DD E::::::::::::::::::::EX:::::X       X:::::X'
+echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::N        N::::::ND::::::::::::DDD   E::::::::::::::::::::EX:::::X       X:::::X'
+echo 'KKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNNDDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEEXXXXXXX       XXXXXXX'
 echo $'\n'
 
 # * Cowsay                                                                    
