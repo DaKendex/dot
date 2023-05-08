060d5decc0d2f3f393afdcc05f67b26102c194a0
Kenji Freitas <kfreitas@league.com>
2023-05-08 17:08:40 -0400

    added alacritty and alias.zsh

--- scripts/motd/motd.sh
+++ scripts/motd/motd.sh
@@ -2,23 +2,16 @@
 # ln -sf ${dotfiledir}/scripts/motd/motd.sh ${homedir}/.config/motd.sh
 
 # * ASCII head
-# https://patorjk.com/software/taag/#p=display&f=Doh&t=KENDEX                                                                                                                   
-echo 'KKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNDDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEEXXXXXXX       XXXXXXX'
-echo 'K:::::::K    K:::::KE::::::::::::::::::::EN:::::::N       N::::::ND::::::::::::DDD   E::::::::::::::::::::EX:::::X       X:::::X'
-echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::::N      N::::::ND:::::::::::::::DD E::::::::::::::::::::EX:::::X       X:::::X'
-echo 'K:::::::K   K::::::KEE::::::EEEEEEEEE::::EN:::::::::N     N::::::NDDD:::::DDDDD:::::DEE::::::EEEEEEEEE::::EX::::::X     X::::::X'
-echo 'KK::::::K  K:::::KKK  E:::::E       EEEEEEN::::::::::N    N::::::N  D:::::D    D:::::D E:::::E       EEEEEEXXX:::::X   X:::::XXX'
-echo '  K:::::K K:::::K     E:::::E             N:::::::::::N   N::::::N  D:::::D     D:::::DE:::::E                X:::::X X:::::X   '
-echo '  K::::::K:::::K      E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N  D:::::D     D:::::DE::::::EEEEEEEEEE       X:::::X:::::X    '
-echo '  K:::::::::::K       E:::::::::::::::E   N::::::N N::::N N::::::N  D:::::D     D:::::DE:::::::::::::::E        X:::::::::X     '
-echo '  K:::::::::::K       E:::::::::::::::E   N::::::N  N::::N:::::::N  D:::::D     D:::::DE:::::::::::::::E        X:::::::::X     '
-echo '  K::::::K:::::K      E::::::EEEEEEEEEE   N::::::N   N:::::::::::N  D:::::D     D:::::DE::::::EEEEEEEEEE       X:::::X:::::X    '
-echo '  K:::::K K:::::K     E:::::E             N::::::N    N::::::::::N  D:::::D     D:::::DE:::::E                X:::::X X:::::X   '
-echo 'KK::::::K  K:::::KKK  E:::::E       EEEEEEN::::::N     N:::::::::N  D:::::D    D:::::D E:::::E       EEEEEEXXX:::::X   X:::::XXX'
-echo 'K:::::::K   K::::::KEE::::::EEEEEEEE:::::EN::::::N      N::::::::NDDD:::::DDDDD:::::DEE::::::EEEEEEEE:::::EX::::::X     X::::::X'
-echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::N       N:::::::ND:::::::::::::::DD E::::::::::::::::::::EX:::::X       X:::::X'
-echo 'K:::::::K    K:::::KE::::::::::::::::::::EN::::::N        N::::::ND::::::::::::DDD   E::::::::::::::::::::EX:::::X       X:::::X'
-echo 'KKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNNDDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEEXXXXXXX       XXXXXXX'
+# https://patorjk.com/software/taag/#p=display&f=Doh&t=KENDEX                                                                                                  
+
+ 
+
+echo '________            ____  __.                 .___              '
+echo '\______ \ _____    |    |/ _|____   ____    __| _/____ ___  ___ '
+echo ' |    |  \\__  \   |      <_/ __ \ /    \  / __ |/ __ \\  \/  / '
+echo ' |    `   \/ __ \_ |    |  \  ___/|   |  \/ /_/ \  ___/ >    <  '
+echo '/_______  (____  / |____|__ \___  >___|  /\____ |\___  >__/\_ \ '
+echo '        \/     \/          \/   \/     \/      \/    \/      \/ '
 echo $'\n'
 
 # * Cowsay                                                                    
