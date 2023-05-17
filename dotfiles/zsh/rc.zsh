6b9624cd0ea34b343a7995db7a301fbdb95cad40
Kenji Freitas <114498344+kfreitas-league@users.noreply.github.com>
2023-05-17 16:52:49 -0400

    update zshrc

--- dotfiles/zsh/rc.zsh
+++ dotfiles/zsh/rc.zsh
@@ -19,10 +19,6 @@ sh_if_exists () {
   fi
 }
 
-source_if_exists $HOME/.env.sh
-source_if_exists $DOTFILES/zsh/alias.zsh
-source_if_exists $ZSH/oh-my-zsh.sh
-
 
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
@@ -35,13 +31,18 @@ export PATH=$HOME/.local/bin:$PATH
 # Gcloud path setup
 export PATH=$PATH:"$(dirname $(readlink -f $(command -v gcloud)))"
 source_if_exists ~/.config/gcloud/.generate_cloud_configs.zshrc
-# source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
-# source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
+source_if_exists "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+source_if_exists "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 
 # Go path setup
 export GOPATH=$HOME/go
 export GOROOT="$(brew --prefix golang)/libexec"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+
+
+source_if_exists $HOME/.env.sh
+source_if_exists $DOTFILES/zsh/alias.zsh
+source_if_exists $ZSH/oh-my-zsh.sh
 
 # 1Password setup
 source_if_exists $HOME/.config/op/plugins.sh
