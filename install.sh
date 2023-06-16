75a1b0cbae4a4d7586e85c0f75683e3966a0a3f3
Kenji Freitas <38636956+KenjiFreitas@users.noreply.github.com>
2023-06-16 15:13:51 -0400

    updated

--- install.sh
+++ install.sh
@@ -26,10 +26,13 @@ brew services start yabai
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew install zsh
 
-ln -sf ${dotfiledir}/yabai/yabairc ${homedir}/.config/yabai/yabairc
-ln -sf ${dotfiledir}/skhd/skhdrc ${homedir}/.config/skhd/skhdrc
+stow -t $HOME zsh
+stow -t $HOME yabai
+stow -t $HOME skhd
+# ln -sf ${dotfiledir}/yabai/yabairc ${homedir}/.config/yabai/yabairc
+# ln -sf ${dotfiledir}/skhd/skhdrc ${homedir}/.config/skhd/skhdrc
 ln -sf ${dotfiledir}/karabiner/karabiner.json ${homedir}/.config/karabiner/karabiner.json
-ln -sf ${dotfiledir}/.zshrc ${homedir}/.zshrc
+# ln -sf ${dotfiledir}/.zshrc ${homedir}/.zshrc
 ln -sf ${dotfiledir}/.p10k.zsh ${homedir}/.p10k.zsh
 ln -sf ${dotfiledir}/.vimrc ${homedir}/.vimrc
 ln -sf ${dotfiledir}/starship/starhip.toml ${homedir}/.config/starship.toml
