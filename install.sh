700fe6794cab9c2f94a60ece417390fd602e97a8
Kenji Freitas <114498344+kfreitas-league@users.noreply.github.com>
2023-08-17 13:54:06 -0400

    change order, add zap

--- install.sh
+++ install.sh
@@ -13,23 +13,20 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Brew Packages
 brew bundle install --file ~/repo/computer-setup/Brewfile
 
+# Install Zap for zsh
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
 # Mac OS
 defaults write com.apple.dock autohide-delay -float 0; defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
 
-# Yabai
-sudo yabai --install-sa
-sudo yabai --load-sa
-brew services start skhd
-brew services start yabai
 
-
+# Link dotfiles
 stow -t $HOME zsh
 stow -t $HOME yabai
 stow -t $HOME skhd
 stow -t $HOME karabiner
 stow -t $HOME starship
 ln -sf ${dotfiledir}/scripts/motd/motd.sh ${homedir}/.config/motd.sh
-
 
 source ~/.zshrc
 
@@ -39,3 +36,9 @@ brew autoupdate start --upgrade
 # dadjoke
 cp /scripts/dadjoke/dadjoke /usr/local/bin/dadjoke
 chmod +x /usr/local/bin/dadjoke
+
+# Yabai
+sudo yabai --install-sa
+sudo yabai --load-sa
+brew services start skhd
+brew services start yabai
