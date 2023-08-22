2d1691549c10802f3037b66a2d22af81e1cdc299
Kenji Freitas <114498344+kfreitas-league@users.noreply.github.com>
2023-08-22 14:40:57 -0400

    update

--- dotfiles/lvim/.config/lvim/lua/user/formatting.lua
+++ dotfiles/lvim/.config/lvim/lua/user/formatting.lua
@@ -6,6 +6,7 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "goimports", filetypes = { "go" } },
   { command = "gofumpt", filetypes = { "go" } },
+  { command = "black", filetypes = { "py"} },
 
 }
 
