e20ca346de289a60c4b4b2d0ece3bc93b2d625ff
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2024-05-13 08:40:14 -0400

    obsidian fmt

--- nvim/.config/nvim/lua/user/obsidian.lua
+++ nvim/.config/nvim/lua/user/obsidian.lua
@@ -1,8 +1,8 @@
 local M = {
 	"epwalsh/obsidian.nvim",
 	version = "*",
-  lazy = true,
-  ft = "markdown",
+	lazy = true,
+	ft = "markdown",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
@@ -10,21 +10,25 @@ local M = {
 
 function M.config()
 	require("obsidian").setup({
-			workspaces = {
+		workspaces = {
+			{
+				name = "obsidian",
+				path = "~/repo/obsidian,",
 			},
-			daily_notes = {
-				-- Optional, if you keep daily notes in a separate directory.
-				folder = "timestamps",
-				-- Optional, if you want to change the date format for the ID of daily notes.
-				date_format = "YYYY/MM-MMMM/YYYY-MM-DD-dddd",
-				-- Optional, if you want to change the date format of the default alias of daily notes.
-				-- alias_format = "%B %-d, %Y",
-				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-				template = "templates/daily.md",
-			},
-			ui = {
-				enabled = true,
-			},
+		},
+		daily_notes = {
+			-- Optional, if you keep daily notes in a separate directory.
+			folder = "timestamps",
+			-- Optional, if you want to change the date format for the ID of daily notes.
+			date_format = "YYYY/MM-MMMM/YYYY-MM-DD-dddd",
+			-- Optional, if you want to change the date format of the default alias of daily notes.
+			-- alias_format = "%B %-d, %Y",
+			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+			template = "templates/daily.md",
+		},
+		ui = {
+			enabled = true,
+		},
 	})
 end
 
