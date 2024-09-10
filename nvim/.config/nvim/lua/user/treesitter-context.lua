c891ee2cbe6ecfb0e7c2f942fcd6e1e6bd5e504d
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2024-09-09 21:35:33 -0400

    treesitter context vim surround

---
+++ nvim/.config/nvim/lua/user/treesitter-context.lua
@@ -0,0 +1,10 @@
+local M = {
+	"nvim-treesitter/nvim-treesitter-context",
+	event = { "BufReadPost", "BufNewFile" },
+}
+
+function M.config()
+	require("treesitter-context").setup({})
+end
+
+return M
---
+++ nvim/.config/nvim/lua/user/vim-surround.lua
@@ -0,0 +1,8 @@
+local M = {
+	"tpope/vim-surround",
+	lazy = false,
+}
+
+function M.config() end
+
+return M
