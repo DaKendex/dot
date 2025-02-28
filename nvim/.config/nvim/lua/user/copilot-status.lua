3a4277729471b23972c367803af19caad3caca19
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-02-28 11:56:03 -0500

    re-enable copilot

--- nvim/.config/nvim/lua/user/copilot-status.lua
+++ nvim/.config/nvim/lua/user/copilot-status.lua
@@ -1,23 +1,24 @@
 local M = {
-  "jonahgoldwastaken/copilot-status.nvim",
-  dependencies = {
-    "copilot.vim",
-  },
-  lazy = true,
-  event = "BufReadPost",
+	"jonahgoldwastaken/copilot-status.nvim",
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+		-- "github/copilot.vim",
+	},
+	lazy = true,
+	event = "BufReadPost",
 }
 
 function M.config()
-  require('copilot_status').setup({
-    icons = {
-    idle = " ",
-    error = " ",
-    offline = " ",
-    warning = "𥉉 ",
-    loading = " ",
-  },
-  debug = false,
-})
+	require("copilot_status").setup({
+		icons = {
+			idle = " ",
+			error = " ",
+			offline = " ",
+			warning = "𥉉 ",
+			loading = " ",
+		},
+		debug = false,
+	})
 end
 
 return M
