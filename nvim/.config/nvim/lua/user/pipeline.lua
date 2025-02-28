df9169db4be2619469b202d93d7ef9e12b5f7333
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-02-28 12:57:17 -0500

    adding pipeline

---
+++ nvim/.config/nvim/lua/user/pipeline.lua
@@ -0,0 +1,12 @@
+local M = {
+	"topaxi/pipeline.nvim",
+	build = "make",
+}
+
+function M.config()
+	require("pipeline").setup({})
+end
+
+vim.keymap.set({ "n", "v" }, "<leader>ci", "<cmd>Pipeline<cr>", { desc = "Open pipeline.nvim" })
+
+return M
