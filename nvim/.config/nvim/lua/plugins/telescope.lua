fef9ccea0fe7ac1c08067bd339f21a8ad12f4039
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-03-12 15:29:51 -0400

    remap telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -17,16 +17,16 @@ function M.config()
 
 	vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Find files" }) -- find files in the current directory
 	vim.keymap.set("n", "<leader>ff", function() -- find files in the root directory
-		builtin.find_files({ cwd = vim.fn.expand("%:p:h") })
+		builtin.find_files({})
 	end, { desc = "Find files" })
 	vim.keymap.set("n", "<leader>fF", function() -- grep in the current directory
-		builtin.find_files({})
+		builtin.find_files({ cwd = vim.fn.expand("%:p:h") })
 	end, { desc = "Find files in Root Dir" })
 	vim.keymap.set("n", "<leader>ft", function() -- grep in the root directory
-		builtin.live_grep({ cwd = vim.fn.expand("%:p:h") })
+		builtin.live_grep({})
 	end, { desc = "Live Grep" })
 	vim.keymap.set("n", "<leader>fT", function()
-		builtin.live_grep({})
+		builtin.live_grep({ cwd = vim.fn.expand("%:p:h") })
 	end, { desc = "Live Grep in Root Dir" })
 	vim.keymap.set("n", "<leader>pws", function()
 		local word = vim.fn.expand("<cword>")
