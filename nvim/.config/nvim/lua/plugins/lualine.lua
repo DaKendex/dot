228425cfbfbbdab080484f3ed59795df20ef662d
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-03-17 19:17:14 -0400

    🎨 Switch to catppuccin lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,38 +6,11 @@ local M = {
 	},
 }
 
-local colors = {
-	blue = "#80a0ff",
-	cyan = "#79dac8",
-	black = "#080808",
-	white = "#c6c6c6",
-	red = "#ff5189",
-	violet = "#d183e8",
-	grey = "#303030",
-}
-
-local bubbles_theme = {
-	normal = {
-		a = { fg = colors.black, bg = colors.violet },
-		b = { fg = colors.white, bg = colors.grey },
-		c = { fg = colors.white },
-	},
-
-	insert = { a = { fg = colors.black, bg = colors.blue } },
-	visual = { a = { fg = colors.black, bg = colors.cyan } },
-	replace = { a = { fg = colors.black, bg = colors.red } },
-
-	inactive = {
-		a = { fg = colors.white, bg = colors.black },
-		b = { fg = colors.white, bg = colors.black },
-		c = { fg = colors.white },
-	},
-}
-
 function M.config()
 	require("lualine").setup({
 		options = {
-			theme = bubbles_theme,
+			-- theme = bubbles_theme,
+      theme = "catppuccin",
 			component_separators = "",
 			section_separators = { left = "", right = "" },
 			icons_enabled = true,
