407e8f2fefdddccd2241eb1d68b6cdf5da02a068
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-03-17 19:17:43 -0400

    🎨 Cleanup colorscheme config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,60 +1,61 @@
 local M = {
-	-- "folke/tokyonight.nvim",
-	"catppuccin/nvim",
-	-- "Mofiqul/vscode.nvim",
-	as = "catppuccin",
-	lazy = false,
-	priority = 1000,
+  -- "folke/tokyonight.nvim",
+  "catppuccin/nvim",
+  -- "Mofiqul/vscode.nvim",
+  as = "catppuccin",
+  lazy = false,
+  priority = 1000,
 }
 
 function M.config()
-	-- require("tokyonight").setup({
-	-- 	style = "storm",
-	-- 	transparent = true,
-	-- 	styles = {
-	-- 		comments = "italic",
-	-- 		-- functions = "italic",
-	-- 		-- keywords = "italic",
-	-- 		-- strings = "italic",
-	-- 		-- variables = "italic",
-	-- 	},
-	-- })
-	-- vim.cmd.colorscheme("tokyonight")
+  -- require("tokyonight").setup({
+  -- 	style = "storm",
+  -- 	transparent = true,
+  -- 	styles = {
+  -- 		comments = "italic",
+  -- 		-- functions = "italic",
+  -- 		-- keywords = "italic",
+  -- 		-- strings = "italic",
+  -- 		-- variables = "italic",
+  -- 	},
+  -- })
+  -- vim.cmd.colorscheme("tokyonight")
 
-	require("catppuccin").setup({
-		flavour = "macchiato",
-		transparent_background = true,
-		styles = {
-			-- comments = { "italic" },
-			-- functions = { "italic" },
-			-- keywords = { "italic" },
-			-- strings = { "italic" },
-			-- variables = { "italic" },
-		},
-		no_underline = true,
-		integrations = {
-			harpoon = true,
-			cmp = true,
-			gitsigns = true,
-			telescope = true,
-			which_key = true,
-			neogit = true,
-			mason = true,
-			native_lsp = {
-				enabled = true,
-				virtual_text = {
-					errors = { "italic" },
-					hints = { "italic" },
-					warnings = { "italic" },
-					information = { "italic" },
-				},
-			},
-		},
-	})
-	vim.cmd.colorscheme("catppuccin")
+  require("catppuccin").setup({
+    flavour = "mocha",
+    transparent_background = true,
+    styles = {
+      comments = { "italic" },
+      functions = { "italic"},
+      operators = { "bold" },
+      conditionals = { "italic" },
+      loops = { "italic" },
+      booleans = { "bold", "italic" },
+    },
+    no_underline = true,
+    integrations = {
+      harpoon = true,
+      cmp = true,
+      gitsigns = true,
+      telescope = true,
+      which_key = true,
+      neogit = true,
+      mason = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+      },
+    },
+  })
+  vim.cmd.colorscheme("catppuccin")
 
-	-- require("vscode").setup()
-	-- vim.cmd.colorscheme("vscode")
+  -- require("vscode").setup()
+  -- vim.cmd.colorscheme("vscode")
 end
 
 return M
