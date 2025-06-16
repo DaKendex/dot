959a99a9f9772ec1470fa857ed335f474e204ba4
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-06-16 14:36:47 -0400

    patch(nvim): obsidian and markdown render

--- .config/nvim/lua/plugins/markdown-render.lua
+++ .config/nvim/lua/plugins/markdown-render.lua
@@ -1,15 +1,15 @@
-local M = {
+return {
   "MeanderingProgrammer/render-markdown.nvim",
   cmd = { "RenderMarkdown" },
   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  config = function()
+    require("render_markdown").setup({
+      completions = {
+        blink = {
+          enabled = true
+        }
+      },                                                 -- enable completions with blink
+      file_types = { "markdown", "Avante", "Obsidian" }, -- file types to render
+    })
+  end,
 }
-
-function M.config()
-  require("obsidian").get_client().opts.ui.enable = false
-  vim.api.nvim_buf_clear_namespace(0, vim.api.nvim_get_namespaces()["ObsidianUI"], 0, -1)
-  require("render_markdown").setup({
-    -- Options go here
-  })
-end
-
-return M
