d62a551823fa107a513bcf8d903242b9111ba6e5
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-06-20 15:40:36 -0400

    patch(nvim): cleaning up oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,14 +1,12 @@
 return {
   "stevearc/oil.nvim",
   dependencies =
-  { 'echasnovski/mini.icons', opts = {}, version = false },
+  "echasnovski/mini.icons",
 
   config = function()
     require("oil").setup({
       columns = { "icon" },
       float = {
-        -- 	max_height = 40,
-        -- 	max_width = 120,
         preview_split = "right",
       },
       keymaps = {
@@ -18,10 +16,6 @@ return {
         show_hidden = true,
       },
     })
-    -- vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     vim.keymap.set("n", "-", "<CMD>lua require('oil').open_float()<CR>", { desc = "Open parent directory" })
-    -- vim.keymap.set("n", "-", function()
-    --   vim.cmd("split | Oil")
-    -- end, { desc = "Open parent directory" })
   end,
 }
