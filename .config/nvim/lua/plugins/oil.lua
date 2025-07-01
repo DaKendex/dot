72ed3c463d064136f62ec9f3d0915768c87a495a
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-06-30 20:22:46 -0400

    patch(nvim): oil fmt

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,6 @@
 return {
   "stevearc/oil.nvim",
-  dependencies =
-  "echasnovski/mini.icons",
+  dependencies = "echasnovski/mini.icons",
 
   config = function()
     require("oil").setup({
@@ -11,11 +10,17 @@ return {
       },
       keymaps = {
         ["<S-h>"] = "actions.select_split",
+        ["<leader>o"] = "<CMD>exec '!open ' . expand('%:p')[6:]<CR>",
       },
       view_options = {
         show_hidden = true,
       },
     })
-    vim.keymap.set("n", "-", "<CMD>lua require('oil').open_float()<CR>", { desc = "Open parent directory" })
+    vim.keymap.set(
+      "n",
+      "-",
+      "<CMD>lua require('oil').open_float()<CR>",
+      { desc = "Open parent directory" }
+    )
   end,
 }
