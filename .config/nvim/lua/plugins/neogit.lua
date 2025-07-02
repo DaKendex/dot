5274459621f8de4b1774d7b58067233e8db76e13
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-07-02 16:43:26 -0400

    patch(nvim): neogit keymaps and event

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,21 +1,27 @@
 return {
   "NeogitOrg/neogit",
-  cmd = "Neogit",
+  event = { "BufReadPost", "BufNewFile" },
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
     "nvim-telescope/telescope.nvim", -- optional
   },
-  config = true,
-  vim.keymap.set("n", "<leader>gs", "<cmd>Neogit<CR>", { silent = true, noremap = true }),
-  vim.keymap.set("n", "<leader>gc", "<cmd>Neogit commit<CR>", { silent = true, noremap = true }),
-  vim.keymap.set("n", "<leader>gp", "<cmd>Neogit pull<CR>", { silent = true, noremap = true }),
-  vim.keymap.set("n", "<leader>gP", "<cmd>Neogit push<CR>", { silent = true, noremap = true }),
-  vim.keymap.set(
-    "n",
-    "<leader>gb",
-    "<cmd>Telescope git_branches<CR>",
-    { silent = true, noremap = true }
-  ),
-  vim.keymap.set("n", "<leader>gB", "<cmd>G blame<CR>", { silent = true, noremap = true }),
+  config = function()
+    local map = function(keys, func, desc, mode)
+      mode = mode or { "n" }
+      vim.keymap.set(mode, keys, func, {
+        buffer = vim.api.nvim_get_current_buf(),
+        desc = "[G]it: " .. desc,
+        silent = true,
+        noremap = true,
+      })
+    end
+    map("<leader>gs", "<cmd>Neogit<CR>", "Neogit [S]tatus")
+    map("<leader>gc", "<cmd>Neogit commit<CR>", "Neogit [C]ommit")
+    map("<leader>gp", "<cmd>Neogit pull<CR>", "Neogit [P]ull")
+    map("<leader>gP", "<cmd>Neogit push<CR>", "Neogit [P]ush")
+    map("<leader>gb", "<cmd>Telescope git_branches<CR>", "Telescope Git [B]ranches")
+    -- map("<leader>gB", "<cmd>G blame<CR>", "Git [B]lame")
+    require("neogit").setup({})
+  end,
 }
