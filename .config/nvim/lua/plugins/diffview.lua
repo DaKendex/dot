e9107196507582e6ec333b54f6bcfa66d124ef5b
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-07-03 11:28:04 -0400

    patch(nvim): diffview keymaps

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -1,8 +1,9 @@
-local M = {
+return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<leader>dd", "<cmd>DiffviewOpen<cr>", desc = "[D]iffview Open" },
+    { "<leader>dh", "<cmd>DiffviewFileHistory<cr>", desc = "[D]iffview File [H]istory" },
+    { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "[D]iffview [C]lose" },
+  },
 }
-
-function M.config() end
-
-return M
