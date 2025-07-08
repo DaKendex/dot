bbaf0a65f9bd4567de38e83d3d0462701d4f0c78
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-07-07 21:20:49 -0400

    patch(nvim): rainbow csv

---
+++ .config/nvim/lua/plugins/rainbow-csv.lua
@@ -0,0 +1,12 @@
+return {
+  "mechatroner/rainbow_csv",
+  -- ft = { "csv", "tsv" },
+  -- config = function()
+  --   vim.g.rainbow_csv_autocmd = 1
+  --   vim.g.rainbow_csv_max_columns = 100
+  --   vim.g.rainbow_csv_max_file_size = 1000000
+  --   vim.g.rainbow_csv_delimiters = { ",", ";", "\t" }
+  --   vim.g.rainbow_csv_highlight_current_line = 1
+  --   vim.g.rainbow_csv_highlight_current_column = 1
+  -- end,
+}
