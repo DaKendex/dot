c96ca38077132a805e7ce456c40932b113723c10
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-07-18 16:03:13 -0400

    none(nvim): octo config

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -1,24 +1,21 @@
-local M = {
+return {
   "pwntester/octo.nvim",
   cmd = "Octo",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
-    { "echasnovski/mini.icons", opts = {}, version = false },
+    "echasnovski/mini.icons",
   },
+  config = function()
+    local keymap = vim.keymap.set
+    local opts = { noremap = true, silent = true }
+
+    keymap("n", "<leader>O", "<cmd>Octo<CR>", opts)
+    require("octo").setup({
+      enable_builtin = true,
+      suppress_missing_scope = {
+        projects_v2 = true,
+      },
+    })
+  end,
 }
-
-function M.config()
-  local keymap = vim.keymap.set
-  local opts = { noremap = true, silent = true }
-
-  keymap("n", "<leader>O", "<cmd>Octo<CR>", opts)
-  require("octo").setup({
-    enable_builtin = true,
-    suppress_missing_scope = {
-      projects_v2 = true,
-    },
-  })
-end
-
-return M
