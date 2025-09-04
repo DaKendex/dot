747b55620aac4e3f8db8b53379e1744980d48b95
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2025-09-04 11:57:28 -0400

    minor(nvim): add oil-git

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,26 +1,29 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = "echasnovski/mini.icons",
+  "benomahony/oil-git.nvim",
+  dependencies = {
+    "stevearc/oil.nvim",
+    -- dependencies = "echasnovski/mini.icons",
 
-  config = function()
-    require("oil").setup({
-      columns = { "icon" },
-      float = {
-        preview_split = "right",
-      },
-      keymaps = {
-        ["<S-h>"] = "actions.select_split",
-        ["<leader>o"] = "<CMD>exec '!open ' . expand('%:p')[6:]<CR>",
-      },
-      view_options = {
-        show_hidden = true,
-      },
-    })
+    config = function()
+      require("oil").setup({
+        columns = { "icon" },
+        float = {
+          preview_split = "right",
+        },
+        keymaps = {
+          ["<S-h>"] = "actions.select_split",
+          ["<leader>o"] = "<CMD>exec '!open ' . expand('%:p')[6:]<CR>",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      })
 
-    local map = function(keys, func, desc, mode)
-      mode = mode or { "n" }
-      vim.keymap.set(mode, keys, func, { desc = "Oil: " .. desc })
-    end
-    map("-", "<CMD>lua require('oil').open_float()<CR>", "Open parent directory")
-  end,
+      local map = function(keys, func, desc, mode)
+        mode = mode or { "n" }
+        vim.keymap.set(mode, keys, func, { desc = "Oil: " .. desc })
+      end
+      map("-", "<CMD>lua require('oil').open_float()<CR>", "Open parent directory")
+    end,
+  },
 }
