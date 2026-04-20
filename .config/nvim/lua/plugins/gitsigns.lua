b532f98c15e6b80ea7b22199a244797e6054a4ec
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2026-04-20 18:00:36 -0400

    patch(gitsigns): fix event

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 local M = {
   "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = "BufReadPre",
 }
 M.config = function()
   local map = function(keys, func, desc, mode)
