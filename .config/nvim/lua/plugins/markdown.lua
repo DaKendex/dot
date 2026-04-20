f139280eaea02c02dafa73e6bd27a62601d343c5
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2026-04-20 18:01:23 -0400

    patch(markdown-preview): fix build

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,8 +2,9 @@ local M = {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
   end,
 }
 
