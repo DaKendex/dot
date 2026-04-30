35fee2326c8f02f07bd1a2df29a9aa842ebcef52
Kenji Freitas <38636956+DaKendex@users.noreply.github.com>
2026-04-30 12:07:29 -0400

    patch(gitsigns): update keymap

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -3,34 +3,22 @@ local M = {
   event = "BufReadPre",
 }
 M.config = function()
-  local map = function(keys, func, desc, mode)
-    mode = mode or { "n", "v" }
-    vim.keymap.set(
-      mode,
-      keys,
-      func,
-      { buffer = vim.api.nvim_get_current_buf(), desc = "[G]it: " .. desc }
-    )
-  end
-  map(
-    "gj",
-    "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>zz",
-    "Next Hunk"
-  )
-  map(
-    "gk",
-    "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>zz",
-    "Prev Hunk"
-  )
-  map("ghp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "[H]unk [P]review")
-  map("ghr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "[H]unk [R]eset")
-  map("ghs", ":Gitsigns stage_hunk<CR>", "[H]unk [S]tage")
-  map("<leader>gl", ":Gitsigns blame_line<CR>", "Blame [L]ine")
-  map("<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "[R]eset Buffer")
-  map("ghu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "[H]unk [U]ndo Stage")
-  map("<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", "[G]it [D]iff This")
-
   require("gitsigns").setup({
+    on_attach = function(bufnr)
+      local map = function(keys, func, desc, mode)
+        mode = mode or { "n", "v" }
+        vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "[G]it: " .. desc })
+      end
+      map("gj", "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>zz", "Next Hunk")
+      map("gk", "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>zz", "Prev Hunk")
+      map("ghp", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "[H]unk [P]review")
+      map("ghr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "[H]unk [R]eset")
+      map("ghs", ":Gitsigns stage_hunk<CR>", "[H]unk [S]tage")
+      map("<leader>gl", ":Gitsigns blame_line<CR>", "Blame [L]ine")
+      map("<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "[R]eset Buffer")
+      map("ghu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "[H]unk [U]ndo Stage")
+      map("<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", "[G]it [D]iff This")
+    end,
     signs = {
       add = { text = "+" },
       change = { text = "~" },
